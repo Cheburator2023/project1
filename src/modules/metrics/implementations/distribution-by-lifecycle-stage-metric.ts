9507d065f7e36105afcd0c7fc2ddc8f902b57668
Dmitriy Kravchenko <drkravchenko@vtb.ru>
2025-02-19 15:53:27 +0300

    refactor: update DistributionByLifecycleStageMetric calculation

--- src/modules/metrics/implementations/distribution-by-lifecycle-stage-metric.ts
+++ src/modules/metrics/implementations/distribution-by-lifecycle-stage-metric.ts
@@ -3,11 +3,17 @@ import { DistributionByLifecycleStageModelsMetricResult } from '../interfaces'
 
 export class DistributionByLifecycleStageMetric extends IndependentMetric<DistributionByLifecycleStageModelsMetricResult> {
   calculate(): DistributionByLifecycleStageModelsMetricResult {
+    const filteredModels = this.filterModels(
+      this.models,
+      this.startDate,
+      this.endDate
+    )
+
     // Use a Map to dynamically count occurrences of each model_status
     const lifecycleStages = new Map<string, number>()
 
     // Iterate through all models to count model_status occurrences
-    this.models.forEach((model) => {
+    filteredModels.forEach((model) => {
       const status = model.model_status
 
       if (!status) {
@@ -23,5 +29,25 @@ export class DistributionByLifecycleStageMetric extends IndependentMetric<Distri
 
     // Convert the Map to an array of [status, count] pairs
     return Array.from(lifecycleStages.entries()) as DistributionByLifecycleStageModelsMetricResult
+  }
+
+  private filterModels(
+    models,
+    startDate: string | null,
+    endDate: string | null
+  ) {
+    const { actualStartDate, actualEndDate } = this.getActualDateRange(startDate, endDate)
+
+    return models.filter((model) => {
+      const createDate = model.create_date ? new Date(model.create_date) : null
+
+      /**
+       * 1. Условие: Если "Дата создания" модели входит в выбранный временной срез,
+       *    ТО модель попадает в категорию "Распределение моделей по этапам жцм".
+       */
+      if (this.isWithinDateRange(createDate, actualStartDate, actualEndDate)) {
+        return true
+      }
+    })
   }
 }
