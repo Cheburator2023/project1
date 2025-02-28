b850c48de8c65bba3026c7c4d37420b17ab10034
Nik Elin <67843454+nik1999777@users.noreply.github.com>
2025-02-28 15:20:07 +0300

    fix: can edit allocation

--- src/modules/artefacts/constants/pseudo-artefacts-sources-roles.ts
+++ src/modules/artefacts/constants/pseudo-artefacts-sources-roles.ts
@@ -3,126 +3,126 @@ export const pseudoArtefactsSourcesRoles = [
     artefact_id: 3000,
     roles: {
       sum_rm: [],
-      sum: [],
+      sum: ['business_customer'],
     },
   },
   {
     artefact_id: 3001,
     roles: {
       sum_rm: [],
-      sum: [],
+      sum: ['business_customer'],
     },
   },
   {
     artefact_id: 3002,
     roles: {
       sum_rm: [],
-      sum: [],
+      sum: ['business_customer'],
     },
   },
   {
     artefact_id: 3003,
     roles: {
       sum_rm: [],
-      sum: [],
+      sum: ['business_customer'],
     },
   },
   {
     artefact_id: 3004,
     roles: {
       sum_rm: [],
-      sum: [],
+      sum: ['business_customer'],
     },
   },
   {
     artefact_id: 3005,
     roles: {
       sum_rm: [],
-      sum: [],
+      sum: ['business_customer'],
     },
   },
   {
     artefact_id: 3006,
     roles: {
       sum_rm: [],
-      sum: [],
+      sum: ['business_customer'],
     },
   },
   {
     artefact_id: 3007,
     roles: {
       sum_rm: [],
-      sum: [],
+      sum: ['business_customer'],
     },
   },
   {
     artefact_id: 3008,
     roles: {
       sum_rm: [],
-      sum: [],
+      sum: ['business_customer'],
     },
   },
   {
     artefact_id: 3009,
     roles: {
       sum_rm: [],
-      sum: [],
+      sum: ['business_customer'],
     },
   },
   {
     artefact_id: 3010,
     roles: {
       sum_rm: [],
-      sum: [],
+      sum: ['business_customer'],
     },
   },
   {
     artefact_id: 3011,
     roles: {
       sum_rm: [],
-      sum: [],
+      sum: ['business_customer'],
     },
   },
   {
     artefact_id: 3012,
     roles: {
       sum_rm: [],
-      sum: [],
+      sum: ['business_customer'],
     },
   },
   {
     artefact_id: 3013,
     roles: {
       sum_rm: [],
-      sum: [],
+      sum: ['business_customer'],
     },
   },
   {
     artefact_id: 3014,
     roles: {
       sum_rm: [],
-      sum: [],
+      sum: ['business_customer'],
     },
   },
   {
     artefact_id: 3015,
     roles: {
       sum_rm: [],
-      sum: [],
+      sum: ['business_customer'],
     },
   },
   {
     artefact_id: 3016,
     roles: {
       sum_rm: [],
-      sum: [],
+      sum: ['business_customer'],
     },
   },
   {
     artefact_id: 3017,
     roles: {
       sum_rm: [],
-      sum: [],
+      sum: ['business_customer'],
     },
   },
 ];
