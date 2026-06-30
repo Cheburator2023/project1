b47e0a8c34612e1ba94ccdb9bbb53c0d48987ffa
VGabbasov <VGabbasov@inno.tech>
2026-06-30 10:56:27 +0300

    feat: Добавил IsOptional в DTO

--- src/api/dto/audit-report-export-request.dto.ts
+++ src/api/dto/audit-report-export-request.dto.ts
@@ -1,12 +1,16 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
+import { IsOptional } from 'class-validator';
 
 export class AuditReportExportRequestDto {
   @ApiPropertyOptional({ description: 'Идентификатор шаблона отчета (если применимо)' })
+  @IsOptional()
   template_id?: number;
 
   @ApiPropertyOptional({ description: 'Дата отчета (если применимо)' })
+  @IsOptional()
   date?: string;
 
   @ApiPropertyOptional({ description: 'Фильтры таблицы' })
+  @IsOptional()
   filters?: Record<string, any>;
 }
